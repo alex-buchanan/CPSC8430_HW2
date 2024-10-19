ede87e9068f78d28d6b16c86cfed01436a582ed5
alex-buchanan <alex.buchanan210@gmail.com>
2024-10-19 01:46:38 -0400

    fixing

--- img_classifier.py
+++ img_classifier.py
@@ -23,20 +23,14 @@ class NNModel(nn.Module):
         self.classify = nn.Linear(dsize, self.dict_length)
 
     def forward(self, inputs, caption_length):
-        print(inputs.shape)
         _, (hn, cn) = self.encoder(inputs)
-        # print(hn.shape)
-        # print(cn.shape)
         col_list = torch.tensor(np.zeros((1,self.dict_length)), dtype=torch.float32)
         col_list[0,0] = 1
         d_output,(hn, cn) = self.decoder(col_list, (hn, cn))
-        # print(d_output)
         col_list = torch.concatenate((col_list,d_output), axis=0)
         for n in range(caption_length-2):
-            # print(n)
             d_output,(hn, cn) = self.decoder(d_output, (hn, cn))
             col_list = torch.concatenate((col_list,d_output), axis=0)
-        # print(col_list.shape)
 
         return col_list
 
--- testing_retrieval.py
+++ testing_retrieval.py
@@ -8,13 +8,20 @@ import sys
 import json
 import numpy as np
 
-np.set_printoptions(linewidth=250)
+# Get cpu, gpu or mps device for training.
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
 
 def Get_All_CNN_Data(order) :
 	direc = "training_data//feat"
 	dir_list = os.listdir(direc)
-	data_set_CNN = torch.tensor(np.array([np.load(direc + '/' + f + '.npy') for f in list(order.keys())[:10]]), dtype=torch.float32)
-	# print(data_set_CNN.shape)
+	data_set_CNN = torch.tensor(np.array([np.load(direc + '/' + f + '.npy') for f in list(order.keys())[:25]]), dtype=torch.float32)
 	return data_set_CNN
 
 def training_labels() :
@@ -40,24 +47,17 @@ def convert_to_num_array(dictionary, dict_list):
 	row_list = []
 	j = 0
 	for v in dictionary.values():
-		# print(v)
-		
-		# for c in v:
 		c = v[0]
 		col_list = np.zeros((1,len(dict_list)))
 		col_list[0,0] = 1
 		c = c.split(' ')
-		# print(c)
 		for i in c:
 			i = i.replace('.', '')
 			i = i.replace(',', '')
-			# print(i)
 			if i != '':
-				# print(i)
 				ans = np.zeros((1,len(dict_list)))
 				ans[0,dict_list[i]-1] = 1
 				col_list = np.concatenate((col_list, ans), axis=0)
-		# print(col_list)
 		for i in range(col_list.shape[0],49):
 			ans = np.zeros((1,len(dict_list)))
 			ans[0,3] = 1
@@ -69,11 +69,7 @@ def convert_to_num_array(dictionary, dict_list):
 		j+=1
 
 	ret_tensor = torch.tensor(row_list, dtype=torch.float32)
-	#ret_tensor = num_list[0]
-	#for n in num_list[1:]:
-	#	ret_tensor = torch.stack((ret_tensor,n), dim=-1)
-	# print(ret_tensor.shape)
-	# print(torch.transpose(ret_tensor,0,1))
+
 	return ret_tensor
 
 captioner = d.NNModel(2039, 4096, 2039, 1)
@@ -87,73 +83,37 @@ CNN_data = Get_All_CNN_Data(t_labels)
 diclist = load_dict()
 q = list(t_labels.values())
 num_vocab = convert_to_num_array(t_labels, diclist)
-# print("vocab shape: " + str(num_vocab.shape))
 
 def train(epoch, data, vocab, model, loss_fcn, optimizer, loss_, accy_):
 	i = 0
 	for conv_image in data:
-		# print(conv_image.shape)
 		model.train()
 		train_loss = 0.0
 
 		# Calculate Probabilities
 		probs = model(conv_image, 50)
-		# print(probs.shape)
-		# print(vocab.shape)
 
 		# Calculate loss, and backpropagate
 		loss = loss_fcn(probs, vocab[i,:,:])
 		train_loss += loss.item()
 
-		
 		loss.backward()
 		optimizer.step()
 		optimizer.zero_grad()
 
 		i+=1
 
-		# if i % 10 == 0:
 		loss, current = loss.item(), i
 		print(f"loss: {loss:>7f}  [{current:>3d}/{len(data):>3d}]")
-
-def test(epoch, data, vocab, model, loss_fcn, optimizer, dict_):
-	model.eval()
-	test_loss, correct = 0,0
-	with torch.no_grad():
-		i = 0
-		for conv_image in data:
-			# print(conv_image.shape)
-			probs = model(conv_image, 50)
-			test_loss += loss_fcn(probs, vocab[i,:,:]).item()
-			phrase = []
-			for p in probs:
-				# print(p)
-				i = p.argmax(0).item()
-				# print(i)
-				w = list(dict_.keys())[list(dict_.values()).index(i+1)]
-				# print(w)
-				phrase.append(w)
-
-			print("image: " + str(i) + " phrase: " + ' '.join(phrase))
-			i+=1
-
-
-
 
 
 train_loss = []
 train_accy = []
 
-
-
 tgs = list(t_labels.keys())
 
-#for i in range(len(tgs)):
-#	print(tgs[i])
-#	print("\t"+str(num_vocab[:,:,i]))
-
-for epoch in range(0, 5) :
+for epoch in range(0, 20) :
 	print(f"Epoch {epoch+1}\n-------------------------------")
 	train(epoch, CNN_data, num_vocab, captioner, criterion, Adam, train_loss, train_accy)
 
-test(epoch, CNN_data, num_vocab, captioner, criterion, Adam, diclist)
+torch.save(captioner.state_dict(), './model_state.pth')
